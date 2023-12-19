a01238b59ebfd49bc594dbac26e035f52425b5d6
reserver7 <reserver7@gmail.com>
2023-12-19 11:36:18 +0900

    mobx, useContext 사용

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import counterStore from "./countStore";
+import { CounterProvider } from "./context/counterContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = new counterStore();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterProvider value={store}>
+      <App />
+    </CounterProvider>
   </React.StrictMode>
 );
 
